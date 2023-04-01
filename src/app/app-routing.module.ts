9666f814d4c5a93e8c17c4f9445367b1ea2551f2
JeanBruneMello <jeanbrunemello@gmail.com>
2023-04-01 17:08:48 -0300

    criação do componente home e roteamento basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: 'full'
+  },
+  {
+path:"home",
+component:HomeComponent
+  },
+  {
+    path: "createItem",
+    component: FormularioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
